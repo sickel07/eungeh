54b4bc694b5391670768263686c2445e30310f24
sickel07 <rlatjsdn726@gmail.com>
2021-01-12 13:15:11 +0900

    Rest

--- src/main/java/com/example/demo/controller/DiaryListController.java
+++ src/main/java/com/example/demo/controller/DiaryListController.java
@@ -2,11 +2,8 @@ package com.example.demo.controller;
 
 import java.util.List;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
 import com.example.demo.Diary;
 import com.example.demo.DiaryListRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -35,41 +32,40 @@ public class DiaryListController {
     }
 
     @RequestMapping("/save")
-    //@RequestMapping(method = RequestMethod.POST)
     public String addToReadingList(Diary diary) {
         diary.setUser(user);
         diaryListRepository.save(diary);
         return "redirect:/";
     }
 
-    @RequestMapping("/delete/{no}")
-    //@RequestMapping(method = RequestMethod.POST)
-    public String deleteList(@PathVariable("no") Long no) {
-        diaryListRepository.delete(diaryListRepository.getOne(no));
-        return "redirect:/";
-    }
-
-    @RequestMapping("/edit/{no}")
+    @GetMapping("/posts/{no}")
     public String editList(@PathVariable("no") Long no, Model model)
     {
         Diary diary = diaryListRepository.getOne(no);
 
         model.addAttribute("diary", diary);
-        return "main/write";
+        return "posts/update";
     }
 
-    @RequestMapping("/update")
+    @PutMapping("/posts/{no}")
     public String updateList(Diary diary)
     {
-        System.out.println(diary.getContent());
         diary.setUser(user);
         diaryListRepository.save(diary);
         return "redirect:/";
     }
 
-    @RequestMapping("/post")
-    //@RequestMapping(method = RequestMethod.POST)
-    public String move() {
-        return "main/write";
+    @RequestMapping("/delete/{no}")
+    public String deleteList(@PathVariable("no") Long no) {
+        diaryListRepository.delete(diaryListRepository.getOne(no));
+        return "redirect:/";
     }
+
+    /*
+    @GetMapping("/posts/delete}")
+    public String deleteList() {
+        //diaryListRepository.delete(diary);
+        return "redirect:/";
+    }
+    */
 }
